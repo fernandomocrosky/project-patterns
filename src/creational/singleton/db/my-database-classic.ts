9384cf9930fb842487a3c95871fc13bc6fbb7fd1
Fernando Mocrosky <fernandobismaia@gmail.com>
2023-08-28 21:12:43 -0300

    Implement builder project pattern

---
+++ src/creational/singleton/db/my-database-classic.ts
@@ -0,0 +1,38 @@
+interface User {
+  name: string;
+  age: number;
+}
+
+export class MyDatabaseClassic {
+  private static _instance: MyDatabaseClassic | null = null;
+  private users: User[] = [];
+
+  private constructor() {}
+
+  public static get instance(): MyDatabaseClassic {
+    if (MyDatabaseClassic._instance === null) {
+      MyDatabaseClassic._instance = new MyDatabaseClassic();
+    }
+    return MyDatabaseClassic._instance;
+  }
+
+  public add(user: User): void {
+    this.users.push(user);
+  }
+
+  public remove(index: number): void {
+    this.users.splice(index, 1);
+  }
+
+  public show(): void {
+    for (const user of this.users) {
+      console.log(user);
+    }
+  }
+}
+
+const myDatabaseClassic = MyDatabaseClassic.instance;
+
+myDatabaseClassic.add({ name: "Myname", age: 0 });
+
+myDatabaseClassic.show();
